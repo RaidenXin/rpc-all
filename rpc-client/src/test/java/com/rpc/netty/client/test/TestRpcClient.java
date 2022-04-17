5a23082126e72c31b6d8d56aa390d5a410bd0701
Raiden <549485630@qq.com>
2022-04-17 22:26:22 +0800

    新增加了一些 测试类

--- rpc-client/src/test/java/com/rpc/netty/client/test/TestRpcClient.java
+++ rpc-client/src/test/java/com/rpc/netty/client/test/TestRpcClient.java
@@ -3,6 +3,7 @@ package com.rpc.netty.client.test;
 import com.rpc.netty.client.RpcClient;
 import com.rpc.netty.client.test.model.ResponseData;
 import com.rpc.netty.core.response.RpcResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -15,6 +16,7 @@ import java.util.Properties;
  * @Date:Created in 18:41 2022/4/17
  * @Modified By:
  */
+@Slf4j
 public class TestRpcClient {
 
     @Test
@@ -25,7 +27,7 @@ public class TestRpcClient {
             Map<String, String> params = new HashMap<>();
             params.put("key", "value");
             RpcResponse<ResponseData> request = RpcClient.request("/test/handler", null, ResponseData.class);
-            System.err.println(request);
+            log.info(request.toString());
         }catch (Exception e){
             e.printStackTrace();
         }
